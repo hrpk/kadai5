76db67b750c67ae14c4ce7c8c21addfa8a27f98e
g15956yh <g15956yh@s30408.tsuda.ac.jp>
2018-05-17 14:19:37 +0900

    first commit ドライバー：hrpk ナビゲーター：etsyk

---
+++ src/kadai5/IntToEng.java
@@ -0,0 +1,22 @@
+package kadai5;
+import java.util.Scanner;
+
+public class IntToEng {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("0から100までの数字を入力してください");
+		Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        System.out.println(translateEng(input));
+
+    }
+
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+        return "";
+    }
+	
+
+}
