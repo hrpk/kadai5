ada32655fe4d5743249e961b32b8ddb676666051
g15904es <g15904es@s20523.tsuda.ac.jp>
2018-05-30 11:54:42 +0900

    second commit

--- src/kadai5/IntToEng.java
+++ src/kadai5/IntToEng.java
@@ -1,22 +1,37 @@
 package kadai5;
 import java.util.Scanner;
+import java.io.*;
 
 public class IntToEng {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("0から100までの数字を入力してください");
-		Scanner sc = new Scanner(System.in);
+    // メインメソッド
+    public static void main(String[] args) {
+		
+	System.out.println("0から99のまでの数字を1つ入力してください。");
+        Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
         System.out.println(translateEng(input));
-
     }
 
-    // 数値を英訳する変換するメソッド
+    // 数値を英訳する変換するメソッドimport java.io.*;
     static String translateEng(int n) {
-        return "";
-    }
+
+	
+	//0~19
+	String[] x = {"zero", "one", "two", "three", "four","five", "six", "seven", "eight", "nine","ten", "eleven", "twelve", "thirteen", "fourteen","fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	
+	//10の倍数
+	String[] y = {"twenty", "thirty", "forty", "fifty", "sixty","seventy", "eighty", "ninety"};
 	
 
+	int a;
+	if(0<=n && n<20){
+	    a = x[n];
+	} else if(n.charAt(1)==0){
+	    a = y[n.charAt(0)-2];
+	}
+	
+        return a.toString();
+    }
 }
